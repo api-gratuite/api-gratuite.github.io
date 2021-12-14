39c06a11c988de93e47b29962155be48339c6725
api-gratuite <94132861+api-gratuite@users.noreply.github.com>
2021-12-14 07:44:52 -0300

    Add files via upload

---
+++ script-home.js
@@ -0,0 +1,161 @@
+if (document.querySelectorAll("img")[0] != undefined) {
+    document.querySelectorAll("img")[0].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[0].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[1] != undefined) {
+    document.querySelectorAll("img")[1].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[1].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[2] != undefined) {
+    document.querySelectorAll("img")[2].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[2].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[3] != undefined) {
+    document.querySelectorAll("img")[3].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[3].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[4] != undefined) {
+    document.querySelectorAll("img")[4].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[4].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[5] != undefined) {
+    document.querySelectorAll("img")[5].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[5].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[6] != undefined) {
+    document.querySelectorAll("img")[6].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[6].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[7] != undefined) {
+    document.querySelectorAll("img")[7].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[7].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[8] != undefined) {
+    document.querySelectorAll("img")[8].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[8].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[9] != undefined) {
+    document.querySelectorAll("img")[9].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[9].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[10] != undefined) {
+    document.querySelectorAll("img")[10].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[10].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[11] != undefined) {
+    document.querySelectorAll("img")[11].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[11].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[12] != undefined) {
+    document.querySelectorAll("img")[12].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[12].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[13] != undefined) {
+    document.querySelectorAll("img")[13].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[13].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[14] != undefined) {
+    document.querySelectorAll("img")[14].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[14].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[15] != undefined) {
+    document.querySelectorAll("img")[15].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[15].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[16] != undefined) {
+    document.querySelectorAll("img")[16].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[16].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[17] != undefined) {
+    document.querySelectorAll("img")[17].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[17].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[18] != undefined) {
+    document.querySelectorAll("img")[18].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[18].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[19] != undefined) {
+    document.querySelectorAll("img")[19].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[19].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[20] != undefined) {
+    document.querySelectorAll("img")[20].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[20].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[21] != undefined) {
+    document.querySelectorAll("img")[21].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[21].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[22] != undefined) {
+    document.querySelectorAll("img")[22].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[22].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[23] != undefined) {
+    document.querySelectorAll("img")[23].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[23].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[24] != undefined) {
+    document.querySelectorAll("img")[24].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[24].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[25] != undefined) {
+    document.querySelectorAll("img")[25].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[25].getAttribute("data-link") + "/1";
+    });
+}
+
+if (document.querySelectorAll("img")[26] != undefined) {
+    document.querySelectorAll("img")[26].addEventListener("click", function () {
+        window.location.href = "/xxx/home/" + document.querySelectorAll("img")[26].getAttribute("data-link") + "/1";
+    });
+}
---
+++ script-nav.js
@@ -0,0 +1,47 @@
+if (document.querySelectorAll("img")[0] != undefined) {
+    document.querySelectorAll("img")[0].addEventListener("click", function () {
+        window.location.href = "/xxx/player/" + document.location.pathname.split("/")[3] + "/" + document.location.pathname.split("/")[4] + "/" + document.querySelectorAll("img")[0].getAttribute("data-link");
+    });
+}
+
+if (document.querySelectorAll("img")[1] != undefined) {
+    document.querySelectorAll("img")[1].addEventListener("click", function () {
+        window.location.href = "/xxx/player/" + document.location.pathname.split("/")[3] + "/" + document.location.pathname.split("/")[4] + "/" + document.querySelectorAll("img")[1].getAttribute("data-link");
+    });
+}
+
+if (document.querySelectorAll("img")[2] != undefined) {
+    document.querySelectorAll("img")[2].addEventListener("click", function () {
+        window.location.href = "/xxx/player/" + document.location.pathname.split("/")[3] + "/" + document.location.pathname.split("/")[4] + "/" + document.querySelectorAll("img")[2].getAttribute("data-link");
+    });
+}
+
+if (document.querySelectorAll("img")[3] != undefined) {
+    document.querySelectorAll("img")[3].addEventListener("click", function () {
+        window.location.href = "/xxx/player/" + document.location.pathname.split("/")[3] + "/" + document.location.pathname.split("/")[4] + "/" + document.querySelectorAll("img")[3].getAttribute("data-link");
+    });
+}
+
+if (document.querySelectorAll("img")[4] != undefined) {
+    document.querySelectorAll("img")[4].addEventListener("click", function () {
+        window.location.href = "/xxx/player/" + document.location.pathname.split("/")[3] + "/" + document.location.pathname.split("/")[4] + "/" + document.querySelectorAll("img")[4].getAttribute("data-link");
+    });
+}
+
+if (document.querySelectorAll("img")[5] != undefined) {
+    document.querySelectorAll("img")[5].addEventListener("click", function () {
+        window.location.href = "/xxx/player/" + document.location.pathname.split("/")[3] + "/" + document.location.pathname.split("/")[4] + "/" + document.querySelectorAll("img")[5].getAttribute("data-link");
+    });
+}
+
+if (document.querySelectorAll("img")[6] != undefined) {
+    document.querySelectorAll("img")[6].addEventListener("click", function () {
+        window.location.href = "/xxx/player/" + document.location.pathname.split("/")[3] + "/" + document.location.pathname.split("/")[4] + "/" + document.querySelectorAll("img")[6].getAttribute("data-link");
+    });
+}
+
+if (document.querySelectorAll("img")[7] != undefined) {
+    document.querySelectorAll("img")[7].addEventListener("click", function () {
+        window.location.href = "/xxx/player/" + document.location.pathname.split("/")[3] + "/" + document.location.pathname.split("/")[4] + "/" + document.querySelectorAll("img")[7].getAttribute("data-link");
+    });
+}
